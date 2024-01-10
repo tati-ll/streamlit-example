210581617bf943d44becc0938a55353b32ec392c
tatiana llana <tjllana@uc.cl>
2024-01-09 22:21:06 -0300

    cambio de estilo para parecerse a spotify

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,32 @@
-import altair as alt
-import numpy as np
 import pandas as pd
 import streamlit as st
 from funciones import playlist_popularity
+
+# Agregar CSS personalizado para imitar el estilo de Spotify
+st.markdown("""
+    <style>
+        body {
+            background-color: #191414;
+            color: #fff;
+            font-family: 'Helvetica Neue', sans-serif;
+        }
+        h1, h2, h3 {
+            color: #1DB954;
+        }
+        select {
+            background-color: #282828;
+            color: #fff;
+        }
+        .stButton {
+            background-color: #1DB954;
+            color: #fff;
+        }
+        .stButton:hover {
+            background-color: #25d366;
+        }
+        /* Puedes agregar más estilos según sea necesario */
+    </style>
+""", unsafe_allow_html=True)
 
 @st.cache_data
 def carga_dataset():
@@ -43,38 +67,16 @@ if st.button("Generar Playlist"):
 
     # Mostrar resultados en Streamlit
     st.write(resultados_playlist[['track_name','track_artist','URI']])
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# """
+# # Welcome to Streamlit!
+
+# Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
+# If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+# forums](https://discuss.streamlit.io).
